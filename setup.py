24afba064a05a9777c964608c03e48610f67ed32
SimpleArt <nguyej17@my.erau.edu>
2022-07-28 23:10:01 -0700

    Added repository link.

--- setup.py
+++ setup.py
@@ -5,10 +5,4 @@ setup(
         "Pretty formatter enables pretty formatting using hanging "
         "indents, dataclasses, ellipses, and simple customizability "
         "by registering formatters.",
-    homepage=
-        "https://github.com/SimpleArt/prettyformatter",
-    repository=
-        "https://github.com/SimpleArt/prettyformatter",
-    summary=
-        "Pretty formatter for prettier formatting.",
 )
